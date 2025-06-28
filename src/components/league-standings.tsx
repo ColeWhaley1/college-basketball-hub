5d20b4811482b5724b13ed3cd8320f138a068f3f
ColeWhaley1 <colewhaley1@gmail.com>
2025-06-28 18:07:58 -0400

    add icons for roster and league standings and add firebase

--- src/components/league-standings.tsx
+++ src/components/league-standings.tsx
@@ -1,8 +1,15 @@
+import { GoTrophy } from "react-icons/go";
+
 export default function LeagueStandings() {
 
     return (
         <div className="bg-gray-100 p-4 rounded-lg w-full">
-            <h1 className="flex items-center">League Standings</h1>
+            <h1 className="flex items-center gap-2">
+                <GoTrophy />
+                <div>
+                    League Standings
+                </div>
+            </h1>
         </div>
     );
 }
--- src/components/your-roster.tsx
+++ src/components/your-roster.tsx
@@ -1,8 +1,12 @@
-export default function YourRoster(){
+import { RiTeamLine } from "react-icons/ri";
+export default function YourRoster() {
 
     return (
         <div className="bg-gray-100 p-4 rounded-lg w-full">
-            <h1 className="flex items-center">Your Roster</h1>
+            <h1 className="flex items-center gap-2">
+                <RiTeamLine />                
+                <div>Your Roster</div>
+            </h1>
         </div>
     );
 }
